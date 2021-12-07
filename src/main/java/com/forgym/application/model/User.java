987a3c9a3f77e705fa35f13eb174f98b3b8886af
PavelJavaStady <PashaJava@yandex.by>
2021-12-07 14:03:08 +0300

    2commit

---
+++ src/main/java/com/forgym/application/model/User.java
@@ -0,0 +1,21 @@
+package com.forgym.application.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+    private boolean enabled;
+
+}
